70839f34cf9089c918c3ffcee25dc648ea73884c
SilverMira <66930495+SilverMira@users.noreply.github.com>
2025-01-07 23:59:17 +0800

    reduce morph weight

--- crates/bevy_mesh/src/morph.rs
+++ crates/bevy_mesh/src/morph.rs
@@ -15,7 +15,7 @@ const MAX_TEXTURE_WIDTH: u32 = 2048;
 const MAX_COMPONENTS: u32 = MAX_TEXTURE_WIDTH * MAX_TEXTURE_WIDTH;
 
 /// Max target count available for [morph targets](MorphWeights).
-pub const MAX_MORPH_WEIGHTS: usize = 128;
+pub const MAX_MORPH_WEIGHTS: usize = 96;
 
 #[derive(Error, Display, Clone, Debug)]
 pub enum MorphBuildError {
